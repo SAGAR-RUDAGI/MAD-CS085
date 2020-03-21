ba9068a2a76b1da0737253a6ee9772cf871a6e6b
SAGAR RUDAGI <sagarrudagi@gmail.com>
2020-03-21 11:05:59 +0530

    Update MainActivity56.java

--- MainActivity56.java
+++ MainActivity56.java
@@ -1,158 +1,137 @@
+MainActivity.java
+
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.AlertDialog.Builder;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
-    EditText Rollno,Name,Marks;
-    Button Insert,Delete,Update,View,ViewAll;
-    SQLiteDatabase db;
+public class MainActivity extends AppCompatActivity{
+    EditText name,marks;
+    Button insert,delete,update,display;
+    DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Rollno=(EditText)findViewById(R.id.Rollno);
-        Name=(EditText)findViewById(R.id.Name);
-        Marks=(EditText)findViewById(R.id.Marks);
-        Insert=(Button)findViewById(R.id.Insert);
-        Delete=(Button)findViewById(R.id.Delete);
-        Update=(Button)findViewById(R.id.Update);
-        View=(Button)findViewById(R.id.View);
-        ViewAll=(Button)findViewById(R.id.ViewAll);
+        myDB = new DatabaseHelper(this);
+        name=(EditText)findViewById(R.id.Name);
+        marks=(EditText)findViewById(R.id.Marks);
+        insert=(Button)findViewById(R.id.Insert);
+        delete=(Button)findViewById(R.id.Delete);
+        update=(Button)findViewById(R.id.Update);
+        display=(Button)findViewById(R.id.Display);
 
-        Insert.setOnClickListener(this);
-        Delete.setOnClickListener(this);
-        Update.setOnClickListener(this);
-        View.setOnClickListener(this);
-        ViewAll.setOnClickListener(this);
-
-        db=openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
-
-    }
-    public void onClick(View view)
-    {
-// Inserting a record to the Student table
-        if(view==Insert)
-        {
-// Checking for empty fields
-            if(Rollno.getText().toString().trim().length()==0||
-                    Name.getText().toString().trim().length()==0||
-                    Marks.getText().toString().trim().length()==0)
-            {
-                showMessage("Error", "Please enter all values");
-                return;
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDB.insert(name.getText().toString(),Integer.parseInt(marks.getText().toString()));
+                Toast.makeText(MainActivity.this, "Inserted..", Toast.LENGTH_LONG).show();
             }
-            db.execSQL("INSERT INTO student VALUES('"+Rollno.getText()+"','"+Name.getText()+
-                    "','"+Marks.getText()+"');");
-            showMessage("Success", "Record added");
-            clearText();
+        });
 
-            // Deleting a record from the Student table
-            if(view==Delete)
-            {
-// Checking for empty roll number
-                if(Rollno.getText().toString().trim().length()==0)
-                {
-                    showMessage("Error", "Please enter Rollno");
-                    return;
-                }
-                Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+Rollno.getText()+"'", null);
-                if(c.moveToFirst())
-                {
-                    db.execSQL("DELETE FROM student WHERE rollno='"+Rollno.getText()+"'");
-                    showMessage("Success", "Record Deleted");
-                }
-                else
-                {
-                    showMessage("Error", "Invalid Rollno");
-                }
-                clearText();
-            }
-
-// Updating a record in the Student table
-            if(view==Update)
-            {
-// Checking for empty roll number
-                if(Rollno.getText().toString().trim().length()==0)
-                {
-                    showMessage("Error", "Please enter Rollno");
-                    return;
-                }
-                Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+Rollno.getText()+"'", null);
-                if(c.moveToFirst()) {
-                    db.execSQL("UPDATE student SET name='"+ Name.getText() + "',marks='"+ Marks.getText() +
-                            "' WHERE rollno='"+Rollno.getText()+"'");
-                    showMessage("Success", "Record Modified");
-                }
-                else{
-                    showMessage("Error", "Invalid Rollno");
-                }
-                clearText();
-            }
-// Display a record from the Student table
-            if(view==View) {
-// Checking for empty roll number
-                if (Rollno.getText().toString().trim().length() == 0) {
-                    showMessage("Error", "Please enter Rollno");
-                    return;
-                }
-                Cursor c = db.rawQuery("SELECT * FROM student WHERE rollno='" + Rollno.getText() + "'", null);
-                if (c.moveToFirst()) {
-                    Name.setText(c.getString(1));
-                    Marks.setText(c.getString(2));
-                } else {
-                    showMessage("Error", "Invalid Rollno");
-                    clearText();
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.Delete) {
+                    myDB.delete(name.getText().toString());
+                    Toast.makeText(MainActivity.this,"Record deleted",Toast.LENGTH_LONG).show();
                 }
             }
+        });
 
-// Displaying all the records
-            if(view==ViewAll)
-            {
-                Cursor c=db.rawQuery("SELECT * FROM student", null);
-                if(c.getCount()==0)
-                {
-                    showMessage("Error", "No records found");
-                    return;
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.Update) {
+                    myDB.update(name.getText().toString(),Integer.parseInt(marks.getText().toString()));
+                    Toast.makeText(MainActivity.this,"Data updated",Toast.LENGTH_LONG).show();
                 }
-
-                StringBuffer buffer=new StringBuffer();
-                while(c.moveToNext())
-                {
-                    buffer.append("Rollno: "+c.getString(0)+"\n");
-                    buffer.append("Name: "+c.getString(1)+"\n");
-                    buffer.append("Marks: "+c.getString(2)+"\n\n");
-                }
-                showMessage("Student Details", buffer.toString());
             }
-        }
-    }
+        });
 
-    public void showMessage(String title,String message){
-        Builder builder=new Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
-    }
-
-    public void clearText()
-    {
-        Rollno.setText("");
-        Name.setText("");
-        Marks.setText("");
-        Rollno.requestFocus();
-
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer det= (StringBuffer) myDB.display();
+                Toast.makeText(MainActivity.this,"Display status "+ det.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+DatabaseHelper.java
+
+package com.example.myapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    public static final String db_name="dept.db";
+    public static final String table_name="student_table";
+    public static final String col1="ID";
+    public static final String col2="Name";
+    public static final String col3="Marks";
+    SQLiteDatabase db;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, db_name, null, 1);
+        db=this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql_query= "create table "+ table_name + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Marks INTEGER)";
+        db.execSQL(sql_query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ table_name);
+        onCreate(db);
+    }
+
+    public void insert(String name, int marks) {
+        String query = " INSERT INTO student_table VALUES (null, "+name+" , "+marks+ ");";
+        db.execSQL(query);
+    }
+
+    public StringBuffer display() {
+        StringBuffer buffer= new StringBuffer();
+        Cursor c=db.rawQuery("SELECT * FROM student_table",null);
+        if(c.getCount()==0){
+            buffer.append(" error no record found");
+            return(buffer);
+        }
+        while(c.moveToNext()){
+            buffer.append("ID : "+ c.getString(0)+ "\n");
+            buffer.append("Name : "+ c.getString(1)+ "\n");
+            buffer.append("Marks : "+ c.getString(2)+ "\n");
+        }return(buffer);
+    }
+    public void update(String name, int marks) {
+        String query = "UPDATE student_table SET Marks=" + marks +
+                " WHERE Name='" + name + "';";
+        db.execSQL(query);
+    }
+    public void delete(String name){
+        String query="DELETE FROM student_table WHERE Name='"+name+"';";
+        db.execSQL(query);}
+}
+
+
